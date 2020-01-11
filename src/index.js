5146caa4484bafefe7e4136ea55f6c9157e8494d
Chris Gutwin <24538491+cgutwin@users.noreply.github.com>
2020-01-11 02:14:15 -0800

    Initial example using mocha and supertest

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-const hello = (keyword) => console.log(`Hello ${keyword}!`)
-hello('world')
+import express from 'express'
+import bodyParser from 'body-parser'
+import http from 'http'
+
+export const app = express()
+export const server = http.createServer(app)
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({
+      "status": "success"
+    })
+})
+
+server.listen(1000, () => console.log(`Listening on port 1000`))
